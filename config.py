e8775e31be7d143a5ff0f7d06817500dd8bd21b2
levi <levischanding1@gmail.com>
2022-03-23 13:51:22 -0400

    Changed title of site

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # encoding: utf-8
 from os import getcwd
-SECRET_KEY='a unique and long key'
-TITLE='Riki' 
-HISTORY_SHOW_MAX=30
+SECRET_KEY = 'a unique and long key'
+TITLE = 'Test Wiki, Please Ignore'
+HISTORY_SHOW_MAX = 30
 PIC_BASE = '/static/content/'
 CONTENT_DIR = f'{getcwd()}/content'
 USER_DIR = f'{getcwd()}/user'
